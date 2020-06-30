1a5b08e5a21e90719414ae71cce616feec2ee45a
dattatray <dattatray111@gmail.com>
2020-06-30 20:08:08 +0530

    changes for class

--- Moneycontrol/src/main/java/StockPrice.java
+++ Moneycontrol/src/main/java/StockPrice.java
@@ -4,9 +4,16 @@ public class StockPrice
 {
 	public static void main(String[] args) throws Exception
 	{
+		for(int i=1;i<10;i++)
+		{
+			for(int j=1;j<i;j++)
+			{
+				System.out.print(" *");
+			}
+			System.out.println();
+		}
 
 	}
-
 
 
 }
