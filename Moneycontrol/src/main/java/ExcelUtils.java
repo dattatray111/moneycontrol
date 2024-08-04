f315965b75d55bf863ca18773860adf1279c3403
datta <datta@host.docker.internal>
2024-08-04 20:25:24 +0530

    Changes added

--- Moneycontrol/src/main/java/ExcelUtils.java
+++ Moneycontrol/src/main/java/ExcelUtils.java
@@ -1,9 +1,10 @@
- 
+
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,20 +23,18 @@ import io.restassured.specification.RequestSpecification;
 public class ExcelUtils {
 	static ArrayList<String> stockcodes = new ArrayList<String>();
 	static Map<String, Object[]> data = new HashMap<String, Object[]>();
-	//static String excelFilePath = "C:\\Users\\Akshay\\Desktop\\stocks.xlsx";
-	static String excelFilePath = "C:\\Users\\Dwaghmod\\OneDrive - Capgemini\\Desktop\\stoock.xlsx";
+	static String excelFilePath = "C:\\Users\\datta\\git\\moneycontrol\\Moneycontrol\\src\\main\\resources\\stocks.xlsx";
 
+	final int sheetNo = 2;
 
-	final int sheetNo= 2;
-	public static void main(String[] args) throws Exception
-	{
-		System.out.println(excelFilePath+"**********");
-			ExcelUtils excel = new ExcelUtils();
-			stockcodes = excel.readExcel();
-			data = excel.getData(stockcodes);
-			excel.writeExcel(data);
+	public static void main(String[] args) throws Exception {
+		System.out.println(excelFilePath + "**********");
+		ExcelUtils excel = new ExcelUtils();
+		stockcodes = excel.readExcel();
+		data = excel.getData(stockcodes);
+		excel.writeExcel(data);
 
-	} 
+	}
 
 	public ArrayList<String> readExcel() throws Exception {
 		try {
@@ -52,10 +51,8 @@ public class ExcelUtils {
 
 			return stockcodes;
 		} catch (Exception e) {
-			throw new Exception("error in reading "+e.getMessage());
+			throw new Exception("error in reading " + e.getMessage());
 		}
-
-
 
 	}
 
@@ -64,13 +61,50 @@ public class ExcelUtils {
 			RestAssured.baseURI = "https://priceapi.moneycontrol.com/pricefeed/nse/equitycash/";
 			RequestSpecification httpRequest = RestAssured.given();
 			Response response = httpRequest.get(stockcodes.get(i).toString());
-			int statusCode = response.getStatusCode();
-			Map<String, String> company = response.jsonPath().getMap("data");
-			System.out.println(company.get("company") + "," + company.get("symbol") + "," + company.get("pricecurrent")
-					+ "," + company.get("52H")+ "," + company.get("52L")+","+company.get("SC_SUBSEC")+company.get("HP")+company.get("LP"));
+			if (response != null) {
+				try {
+				
+					int statusCode = response.getStatusCode();
+					Map<String, String> company = response.jsonPath().getMap("data");
+					System.out.println(company);
+					company.put("symbol", stockcodes.get(i).toString());
+					System.out.println(
+							company.get("SC_FULLNM")+" "+
+									company.get("pricecurrent")+" "+
+									company.get("52H")+" "+
+									company.get("52L")+" "+
+									company.get("200DayAvg")+" "+
+									company.get("newSubsector")
+							);
+					double downBy=0;
+					try
+					{
+						double currentP_int = Double.parseDouble(company.get("pricecurrent"));
+						double High_int = Double.parseDouble(company.get("52H"));
+						
+						downBy =( (High_int-currentP_int)/currentP_int)*100;
+					}
+					
+					catch (Exception e) {
+						// TODO: handle exception
+					}
+					company.put("downBy", String.valueOf(downBy));
+					data.put(stockcodes.get(i).toString(), new Object[] 
+					{ 
+							company.get("symbol"),
+							company.get("SC_FULLNM"),
+							company.get("pricecurrent"),
+							company.get("52H"),
+							company.get("52L"),
+							company.get("DayAvg"),
+							company.get("newSubsector"),
+							company.get("downBy")
+							});
+				} catch (Exception e) {
+					// TODO: handle exception
+				}
 
-			data.put(stockcodes.get(i).toString(), new Object[] { company.get("company"), company.get("symbol"),
-					company.get("pricecurrent"),company.get("SC_SUBSEC"),company.get("HP"),company.get("LP")});
+			}
 
 		}
 		return data;
@@ -80,43 +114,72 @@ public class ExcelUtils {
 	public void writeExcel(Map<String, Object[]> data) throws IOException {
 		FileInputStream inputStream = new FileInputStream(new File(excelFilePath));
 		Workbook workbook = new XSSFWorkbook(inputStream);
-		XSSFSheet spreadsheet = (XSSFSheet) workbook.getSheetAt(sheetNo);
+		XSSFSheet spreadsheet = (XSSFSheet) workbook.getSheet("Result");
 		XSSFRow row;
-
-		for (int rowIndex = 1; rowIndex <= spreadsheet.getLastRowNum(); rowIndex++) {
-			row = spreadsheet.getRow(rowIndex);
-			Cell cell = row.getCell(1);
-			System.out.println(cell.getStringCellValue()+"                     ..........");
-			Object[] objectArr = data.get(cell.getStringCellValue());
-			/*for(int i=0;i<objectArr.length;i++)
+		System.out.println(data.keySet());
+		int rowIndex = 1;
+		
+		
+		for (String symbol : data.keySet()) 
+		{
+			System.out.println("======================> "+symbol);
+			int cellNum = 4;
+			row = spreadsheet.createRow(rowIndex); 
+			Object[] objectArr = data.get(symbol);
+			if(!objectArr.equals(null))
 			{
-				System.out.println(objectArr[i]);
-			}*/
-			int cellid = 4;
-
-			for (int i=0;i<objectArr.length;i++)
-			{
-
-
-				System.out.println();
-				if(i==2||i==3||i==4||i==5||i==6||i==7)
+				System.out.println(Arrays.asList(objectArr));
+				for(int i=0;i<objectArr.length;i++)
 				{
-
-				Cell cell1 = row.createCell(cellid);
-				Object obj = objectArr[i].toString();
-				if (obj instanceof Date)
-		            cell1.setCellValue((Date) obj);
-		        else if (obj instanceof Boolean)
-		            cell1.setCellValue((Boolean) obj);
-		        else if (obj instanceof String)
-		            cell1.setCellValue(obj.toString());
-		        else if (obj instanceof Double)
-		            cell1.setCellValue((Double) obj);
-				cellid++;
-            }
+					
+					Cell cell = row.createCell(cellNum);
+					if(objectArr[i] != null)
+					{
+						cell.setCellValue(objectArr[i].toString());
+						
+					}
+					else
+					{
+						cell.setCellValue("NA");
+					}
+					cellNum++;
+				} 
+				rowIndex++;
 			}
-
 			}
+			
+		/*
+		 * for (int rowIndex = 1; rowIndex <= spreadsheet.getLastRowNum(); rowIndex++) {
+		 * row = spreadsheet.getRow(rowIndex); Cell cell = row.getCell(1);
+		 * System.out.println(cell.getStringCellValue() +
+		 * "                     .........."); Object[] objectArr =
+		 * data.get(cell.getStringCellValue());
+		 * 
+		 * 
+		 * 
+		 * 
+		 * for(int i=0;i<objectArr.length;i++) { System.out.println(objectArr[i]); } int
+		 * cellid = 4; if(objectArr != null) { for (int i=0;i<objectArr.length;i++) {
+		 * 
+		 * 
+		 * System.out.println(); Cell cell1 = row.createCell(cellid); Object obj =
+		 * objectArr[i].toString();
+		 * 
+		 * if(i==2||i==3||i==4||i==5||i==6||i==7) {
+		 * 
+		 * Cell cell1 = row.createCell(cellid); Object obj = objectArr[i].toString(); if
+		 * (obj instanceof Date) cell1.setCellValue((Date) obj.toString()); else if (obj
+		 * instanceof Boolean) cell1.setCellValue((Boolean) obj); else if (obj
+		 * instanceof String) cell1.setCellValue(obj.toString()); else if (obj
+		 * instanceof Double) cell1.setCellValue((Double) obj); cellid++; }
+		 * 
+		 * 
+		 * 
+		 * } }
+		 * 
+		 * 
+		 * }
+		 */
 
 		FileOutputStream out = new FileOutputStream(new File(excelFilePath));
 
